f1e6a34949a20b90b81bb03123680834b425c1de
wangzhenpeng <wangzhenpeng@ygzykj.com>
2021-12-31 15:18:15 +0800

    添加工具类

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -10,22 +10,14 @@
   </div>
 </template>
 <script setup lang="ts">
-import { ref, $Common } from 'vue'
+import { ref, getCurrentInstance } from 'vue'
 import Card from '@/components/card/card.vue'
 import Head from './components/head.vue'
 import Watch from './components/watch.vue'
 
-class record {
-  id: string
-
-  constructor(id) {
-    this.id = id
-  }
-}
-console.log($Common)
-const dataSouce: Array<record> = ref([])
-const hours: Array<number> = [...new Array(12).keys()]
-const now: Date = ref(new Date())
+const { proxy } = getCurrentInstance()
+proxy.$Common.formatDate('123')
+console.log()
 </script>
 <style lang="scss" scoped>
 .body {
